60cd1569161c77fc82f80bc5b321c0b29cb01ae7
Tomas Ferreras <hellotomasdev@gmail.com>
2022-06-27 15:28:28 -0300

    fix: container size issues fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Typography } from '@mui/material';
-import { Container } from '@mui/system';
 import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products/ProductList';
@@ -13,15 +12,13 @@ const HomePage: NextPage = () => {
         'Order Bourbon Online and have it delivered directly to your door. Buy bourbon online today. Large selection of Single Batch Bourbon, Whiskies and more.'
       }
     >
-      <Container maxWidth="lg">
-        <Typography variant="h1" component="h1">
-          Shop
-        </Typography>
-        <Typography variant="h2" sx={{ mb: 1 }}>
-          All products
-        </Typography>
-        <ProductList products={initialData.products as any} />
-      </Container>
+      <Typography variant="h1" component="h1">
+        Shop
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+      <ProductList products={initialData.products as any} />
     </ShopLayout>
   );
 };
