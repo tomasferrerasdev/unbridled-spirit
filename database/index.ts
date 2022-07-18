e8e5549188e34588fa76d8a95945edbc0ecde27d
Tomas Ferreras <hellotomasdev@gmail.com>
2022-07-18 10:53:13 -0300

    feat: verify user email and password credentials

--- database/index.ts
+++ database/index.ts
@@ -1,6 +1,6 @@
 export * from './constants';
 export * as db from './db';
-export * as usersDB from './dbusers';
+export * as dbUsers from './dbUsers';
 export * as productsDB from './productsDB';
 // Testing Database
 export * as seedDatabase from './seed-data';
